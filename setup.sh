ed7d0ac7ad545e2bf4f8715aa48db2d040ea39aa
Wei Wang <wei_company@126.com>
2020-05-25 20:40:04 -0400

    change setup location from bash_aliases to bashrc 2

--- setup.sh
+++ setup.sh
@@ -9,14 +9,14 @@ setupAliases() {
     if (grep -q "git_update_batch" ~/.bashrc); then
         echo "Found bashrc entry: git_update_batch=$SCRIPT_DIR/tools/git_update_batch.sh"
     else
-        echo "alias git_update_batch='${SCRIPT_DIR}/tools/git_update_batch.sh'" >> ~/.bash_aliases
+        echo "alias git_update_batch='${SCRIPT_DIR}/tools/git_update_batch.sh'" >> ~/.bashrc
     fi
     
     # find_git_repo alias
     if (grep -q "find_git_repo" ~/.bashrc); then
         echo "Found bashrc entry: find_git_repo=$SCRIPT_DIR/tools/find_git_repo.sh"
     else
-        echo "alias find_git_repo='${SCRIPT_DIR}/tools/find_git_repo.sh'" >> ~/.bash_aliases
+        echo "alias find_git_repo='${SCRIPT_DIR}/tools/find_git_repo.sh'" >> ~/.bashrc
     fi
     
     # update
