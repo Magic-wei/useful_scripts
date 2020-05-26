d6338549f9a41bf691cf9550348e262c766c3d36
Wei Wang <wei_company@126.com>
2020-05-26 09:52:18 -0400

    Convert tab to 4 spaces

--- tools/export_git_repo_dir.sh
+++ tools/export_git_repo_dir.sh
@@ -4,8 +4,8 @@ set -e  # exit on first error
 SCRIPT_DIR=$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
 main(){
-	source ${SCRIPT_DIR}/find_git_repo.sh $1
-	echo "Found Git repositories: ${GIT_REPO_DIR[*]}"
+    source ${SCRIPT_DIR}/find_git_repo.sh $1
+    echo "Found Git repositories: ${GIT_REPO_DIR[*]}"
 }
 
 main $1
