29833048c7b6db2ab52be960d710cf2a4f3a2365
Wei Wang <wei_company@126.com>
2020-05-31 16:36:38 -0400

    Update latest commands of gnome-terminal.

--- examples/open_multiple_terminal.sh
+++ examples/open_multiple_terminal.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # By default, use 'gnome-terminal' command to create a new terminal:
-# > gnome-terminal --tab "Terminal Title" -x bash -c "your commands;exec /bin/bash"
-# You can also try other terminal like terminator, it's the same: 
+# > gnome-terminal --tab -- bash -ic "your commands;exec /bin/bash"
+# # we use '-i' for interactive bash to source ~/.bashrc automatically when start a new terminal.
+# You can also try other terminal like terminator (need to be installed first): 
 # > terminator --title "title" -x bash -c "your commands;exec /bin/bash"
 # This script provide an example to craete two terminals one-by-one with 1 second interval 
 # and create folders and files under the script dir.
@@ -12,18 +13,18 @@ SCRIPT_DIR=$( cd $( dirname $(readlink -f ${BASH_SOURCE[0]}) ) && pwd )
 DIR1="open_multiple_terminal1"
 DIR2="open_multiple_terminal2"
 
-gnome-terminal --tab "terminal1" -x bash -c "cd $SCRIPT_DIR; \
-                                             mkdir $DIR1; \
-                                             cd $DIR1; \
-                                             touch $DIR1.txt; \
-                                             echo '$DIR1.txt created!'; \
-                                             exec /bin/bash"
+gnome-terminal --tab -- bash -ic "cd $SCRIPT_DIR; \
+                                  mkdir $DIR1; \
+                                  cd $DIR1; \
+                                  touch $DIR1.txt; \
+                                  echo '$DIR1.txt created!'; \
+                                  exec /bin/bash"
 
-sleep 1s
+sleep 1s # 1s for 1 second, 0.5m for 0.5 minute
 
-gnome-terminal --tab "terminal2" -x bash -c "cd $SCRIPT_DIR; \
-                                             mkdir $DIR2; \
-                                             cd $DIR2; \
-                                             touch $DIR2.csv; \
-                                             echo '$DIR2.txt created!'; \
-                                             exec /bin/bash"
+gnome-terminal --tab -- bash -ic "cd $SCRIPT_DIR; \
+                                  mkdir $DIR2; \
+                                  cd $DIR2; \
+                                  touch $DIR2.csv; \
+                                  echo '$DIR2.txt created!'; \
+                                  exec /bin/bash" # keep the current tab open after all cmds executed
