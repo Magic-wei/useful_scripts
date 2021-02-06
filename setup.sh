aa7ae2ac23ddeebe9f6f7bd06c11dfb35347aa63
Wei Wang <wei_company@126.com>
2021-02-05 22:47:44 -0500

    Improve gitcd & Refactor all scripts

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -e  # exit on first error
 
 SCRIPT_DIR=$( cd $( dirname $(readlink -f ${BASH_SOURCE[0]}) ) && pwd )
 BIN_DIR=$HOME/bin
+BASHRC_PATH=${HOME}/.bashrc
 
 setupBin(){
     if [ ! -d $BIN_DIR ]; then
@@ -33,14 +34,6 @@ setupBin(){
         ln -sfn $SCRIPT_DIR/tools/$APP_NAME $BIN_DIR/$APP_NAME
     fi
 
-    # gitcd
-    APP_NAME="gitcd"
-    if [ -f $BIN_DIR/$APP_NAME ]; then
-        echo "Found bin entry: $BIN_DIR/$APP_NAME"
-    else
-        ln -sfn $SCRIPT_DIR/tools/$APP_NAME $BIN_DIR/$APP_NAME
-    fi
-
     # echo_with_color
     APP_NAME="echo_with_color"
     if [ -f $BIN_DIR/$APP_NAME ]; then
@@ -49,49 +42,68 @@ setupBin(){
         ln -sfn $SCRIPT_DIR/tools/$APP_NAME $BIN_DIR/$APP_NAME
     fi
 
-    echo "setup to $BIN_DIR successfully!"
+    # gitcd
+    APP_NAME="gitcd"
+    if (grep -q "${APP_NAME}" ${BASHRC_PATH}); then
+        echo "Found bashrc entry: . ${SCRIPT_DIR}/tools/${APP_NAME}"
+    else
+        echo ". ${SCRIPT_DIR}/tools/${APP_NAME}" >> ${BASHRC_PATH}
+    fi
+
+    # update
+    source ${BASHRC_PATH}
+
+    echo "setup to $HOME/.bashrc successfully! (Except gitcd that requires to be installed to bashrc)"
 }
 
 setupBashrc() {
-    # find_all_folders alias
+    # find_all_folders
     APP_NAME="find_all_folders"
-    if (grep -q "${APP_NAME}" ~/.bashrc); then
+    if (grep -q "${APP_NAME}" ${BASHRC_PATH}); then
         echo "Found bashrc entry: ${APP_NAME}=$SCRIPT_DIR/tools/${APP_NAME}"
     else
-        echo "alias ${APP_NAME}='${SCRIPT_DIR}/tools/${APP_NAME}'" >> ~/.bashrc
+        echo "alias ${APP_NAME}='${SCRIPT_DIR}/tools/${APP_NAME}'" >> ${BASHRC_PATH}
     fi
 
-    # git_update_batch alias
+    # git_update_batch
     APP_NAME="git_update_batch"
-    if (grep -q "${APP_NAME}" ~/.bashrc); then
+    if (grep -q "${APP_NAME}" ${BASHRC_PATH}); then
         echo "Found bashrc entry: ${APP_NAME}=$SCRIPT_DIR/tools/${APP_NAME}"
     else
-        echo "alias ${APP_NAME}='${SCRIPT_DIR}/tools/${APP_NAME}'" >> ~/.bashrc
+        echo "alias ${APP_NAME}='${SCRIPT_DIR}/tools/${APP_NAME}'" >> ${BASHRC_PATH}
     fi
     
-    # find_git_repo alias
+    # find_git_repo
     APP_NAME="find_git_repo"
-    if (grep -q "${APP_NAME}" ~/.bashrc); then
+    if (grep -q "${APP_NAME}" ${BASHRC_PATH}); then
         echo "Found bashrc entry: ${APP_NAME}=$SCRIPT_DIR/tools/${APP_NAME}"
     else
-        echo "alias ${APP_NAME}='${SCRIPT_DIR}/tools/${APP_NAME}'" >> ~/.bashrc
+        echo "alias ${APP_NAME}='${SCRIPT_DIR}/tools/${APP_NAME}'" >> ${BASHRC_PATH}
     fi
 
-    # echo_with_color alias
+    # echo_with_color
     APP_NAME="echo_with_color"
-    if (grep -q "${APP_NAME}" ~/.bashrc); then
+    if (grep -q "${APP_NAME}" ${BASHRC_PATH}); then
         echo "Found bashrc entry: ${APP_NAME}=$SCRIPT_DIR/tools/${APP_NAME}"
     else
-        echo "alias ${APP_NAME}='${SCRIPT_DIR}/tools/${APP_NAME}'" >> ~/.bashrc
+        echo "alias ${APP_NAME}='${SCRIPT_DIR}/tools/${APP_NAME}'" >> ${BASHRC_PATH}
     fi
-    
+
+    # gitcd
+    APP_NAME="gitcd"
+    if (grep -q "${APP_NAME}" ${BASHRC_PATH}); then
+        echo "Found bashrc entry: . ${SCRIPT_DIR}/tools/${APP_NAME}"
+    else
+        echo ". ${SCRIPT_DIR}/tools/${APP_NAME}" >> ${BASHRC_PATH}
+    fi
+
     # update
-    source ~/.bashrc
+    source ${BASHRC_PATH}
 
     echo "setup to $HOME/.bashrc successfully!"
 }
 
-main() {
+setup() {
     if [ $# = 0 ]; then
         setupBin
     fi
@@ -102,4 +114,4 @@ main() {
     esac
 }
 
-main $@
+setup $@
